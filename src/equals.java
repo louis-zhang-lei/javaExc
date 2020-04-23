02e762eacf03341c7af996409bd6cde355705bdf
1335926665@qq.com <qaz753>
2020-04-23 13:06:09 +0800

    Commit Message

---
+++ src/equals.java
@@ -0,0 +1,36 @@
+public class equals {
+    public static void main(String[] args) {
+        int a=1;
+        int b=3;
+        char c=65;
+        double d=(float)a;
+        int i=1;
+        int j;
+        j=i++*2+3*--i;
+
+
+
+        int x=2;
+        switch (x){
+            case 1:
+                System.out.println(1);break;
+            case 2:
+
+            case 3:
+                System.out.println(3);break;
+        }
+
+//        String x;
+//        int x=1;
+//        int y=10;
+//        while (y-->0){
+//            x++;
+//        }
+//        System.out.println(x+" "+y);
+//        if (a==b){
+//            System.out.println(1);
+//        }
+//        System.out.println(x);
+//        System.out.println(str.equals(str1));
+    }
+}
---
+++ src/toString.java
@@ -0,0 +1,14 @@
+import java.lang.StringBuffer;
+public class toString {
+    public static void main(String[] args) {
+        double a=1;
+        Integer b=new Integer(2);
+        String c="3";
+        StringBuffer d=new StringBuffer("4");
+
+        System.out.println(a);
+        System.out.println(b.toString());
+        System.out.println(c.toString());
+        System.out.println(d.toString());
+    }
+}
